3c75f561f8e2daa82165a7083871d10be9bff036
nickenv <nitesh16f23@gmail.com>
2026-01-18 20:17:02 +0530

    Created working Restfull API

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
-app.post("/esp32", (req, res) => {
-  console.log("ESP32 data:", req.body);
-  res.json({ status: "ok" });
+app.get('/tshirt', (req, res)=> {
+  res.status(200).send({
+    tshirt: 'polo',
+    size: 'large',
+    tag: '1234'
+  })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: 'we need logo!'})
+  }
+
+  res.send({
+    tshirt: ` tshirt with your ${logo} and ID of ${id}`,
+  });
+
 });
 
 app.get("/", (req, res) => {
-  res.send("Node.js server is running OK, fucking cool project oh yeahhhhhhhhh!!!!!");
+  res.send(`Node.js server is running OK on http://localhost:${PORT}`);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server listening on port 3000");
 });
